7721408462868910245fa57dd272a576675d87bf
Chris Ford <christophertford@gmail.com>
2018-05-12 11:37:20 +0200

    Add initial set of empty Clojure files.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject it-aint-necessarily-so "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [overtone "0.9.1"]
+                 [leipzig "0.9.0"]])
---
+++ src/so/necessarily.clj
@@ -0,0 +1,6 @@
+(ns so.necessarily
+  (:require [overtone.live :refer :all :exclude [stop scale]]
+            [leipzig.melody :refer :all]
+            [leipzig.scale :refer [scale high low from]]
+            [leipzig.live :as live]
+            [leipzig.live :refer [stop]]))
