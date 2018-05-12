1b0462c01a119957faa1b135d8955eda2de0ac59
Chris Ford <christophertford@gmail.com>
2018-05-12 11:44:54 +0200

    Play a scale.

--- src/so/necessarily.clj
+++ src/so/necessarily.clj
@@ -1,6 +1,32 @@
 (ns so.necessarily
   (:require [overtone.live :refer :all :exclude [stop scale]]
             [leipzig.melody :refer :all]
-            [leipzig.scale :refer [scale high low from]]
+            [leipzig.scale :refer [A minor scale high low from]]
             [leipzig.live :as live]
             [leipzig.live :refer [stop]]))
+
+(def melody (phrase (repeat 1) (range 0 8) ))
+
+(def piece
+  (->> melody
+       (where :time (bpm 90))
+       (where :pitch (comp A minor))))
+
+(comment
+  (live/play piece)
+  )
+
+(definst overchauffeur [freq 110 dur 1.0 top 2500 vol 0.25]
+  (-> (sin-osc freq)
+      (+ (* 1/3 (sin-osc (* 2.01 freq))))
+      (+ (* 1/2 (sin-osc (* 3.01 freq))))
+      (+ (* 1/8 (sin-osc (* 5.01 freq))))
+      (+ (* 2 (sin-osc (* 0.5 freq))))
+      (clip2 0.7)
+      (lpf top)
+      (hpf 20)
+      (* (env-gen (adsr 0.01 0.2 0.8 0.2) (line:kr 1 0 dur) :action FREE))
+      (* vol)))
+
+(defmethod live/play-note :default [{:keys [pitch duration]}]
+  (overchauffeur :freq (midi->hz pitch) :dur duration))
