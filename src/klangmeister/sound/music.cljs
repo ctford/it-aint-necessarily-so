c9bb7a7582d76de26cf1adad78aed8afdfab0913
Chris Ford <christophertford@gmail.com>
2018-07-03 00:20:09 +0200

    Make bell longer.

--- src/klangmeister/sound/music.cljs
+++ src/klangmeister/sound/music.cljs
@@ -6,7 +6,7 @@
   [audiocontext from notes]
   (doseq [{:keys [time instrument] :as note} notes]
     (let [at (+ time from)
-          duration 3.0
+          duration 4.0
           synth-instance (-> note
                              (dissoc :time)
                              instrument)
